36059a40bcc2ca42c06dcc112f4c3894c1f83794
Shannon Beach <shannonbeach@Shannons-MBP-2.attlocal.net>
2017-10-15 11:06:11 -0400

    added sqlite connection

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Page struct {
-	Name string
+	Name     string
+	DBStatus bool
 }
 
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
+	db, _ := sql.Open("sqlite3", "dev.db")
+
+	//route handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+
+		p.DBStatus = db.Ping() == nil
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
